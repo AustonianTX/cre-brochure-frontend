f8fc36a11abfefc8b274379dd91e0721177a5d79
Austin Johnson <austin@austinjohnson.me>
2023-02-22 13:48:07 -0600

    updated message

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const LoadingSpinner = () => {
+
+  // test
+
   return (
     <div className="flex items-center justify-center px-2 ">
       <div className="w-8 h-8 border-b-2 border-gray-500 rounded-full animate-spin"></div>

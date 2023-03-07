4f279e0ed637b03ed31358c7187c1ddf06e704d5
Austin Johnson <austin@austinjohnson.me>
2023-03-07 13:39:31 -0600

    building layouts

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import "../styles/globals.css";
+
+/* eslint-disable @next/next/no-img-element */
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+
+export const metadata = {
+  title: "AI CRE Demos",
+  description:
+    "These are basic demos which use AI to build real estate data from brochure style text and files.",
+};
+
+const user = {
+  name: "Tom Cook",
+  email: "tom@example.com",
+  imageUrl:
+    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
+const navigation = [
+  { name: "Text Parse Demo", href: "#", current: true },
+  { name: "Brochure Upload Demo", href: "#", current: false },
+];
+
+const userNavigation = [
+  { name: "Your Profile", href: "#" },
+  { name: "Settings", href: "#" },
+  { name: "Sign out", href: "#" },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="h-full bg-gray-100">
+      <body className="h-full">{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Home from "./home-page";
+
+export const metadata = {
+  title: "AI CRE Demos",
+};
+
+export default function Page() {
+  return <Home />;
+}
